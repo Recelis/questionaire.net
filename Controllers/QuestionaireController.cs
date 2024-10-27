f1a6f2802c8f11babb4e4949ff254b8bb78153dd
Recelis <jackymlui@live.com>
2024-10-27 22:44:48 +1000

    setting up ASP.NET project and creation of Models folder with controller rename

---
+++ Controllers/QuestionaireController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using QuestionaireModels;
+
+namespace Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class QuestionaireController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<QuestionaireController> _logger;
+
+    public QuestionaireController(ILogger<QuestionaireController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<Questionaire> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new Questionaire
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
